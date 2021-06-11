9f699747020ce41817c3aded8d247eb6f86fa1f0
Ary Vidot <aryvid@gmail.com>
2021-06-11 22:52:03 +0100

    hero and About section styled

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
 "use strict";
+// Nav Toggle
+const navToggle = document.querySelector(".nav-toggle");
+const nav = document.querySelector(".nav");
+const navItem = document.querySelectorAll(".nav__item");
+
+navToggle.addEventListener("click", () => {
+  nav.classList.toggle("nav--visible");
+  // nav.classList.toggle("nav--toggled");
+});
+navItem.addEventListener("click", () => {
+  nav.classList.remove("nav--visible");
+  // nav.classList.remove("nav--toggled");
+});
 
 // MODAL
 const modal = document.querySelector(".modal");
