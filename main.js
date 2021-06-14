557fd7462cf0767e2432dbca08f77ddb716a8ae3
Ary Vidot <aryvid@gmail.com>
2021-06-14 11:39:02 +0100

    Added Testimonial, Contact form sections

--- main.js
+++ main.js
@@ -14,32 +14,32 @@ navItem.addEventListener("click", () => {
 });
 
 // MODAL
-const modal = document.querySelector(".modal");
-const overlay = document.querySelector(".overlay");
-const btnCloseModal = document.querySelector(".close-modal");
-const btnsOpenModal = document.querySelectorAll(".show-modal");
+// const modal = document.querySelector(".modal");
+// const overlay = document.querySelector(".overlay");
+// const btnCloseModal = document.querySelector(".close-modal");
+// const btnsOpenModal = document.querySelectorAll(".show-modal");
 
-const openModal = function () {
-  // console.log('Button clicked');
-  modal.classList.remove("hidden");
-  overlay.classList.remove("hidden");
-};
+// const openModal = function () {
+//   // console.log('Button clicked');
+//   modal.classList.remove("hidden");
+//   overlay.classList.remove("hidden");
+// };
 
-const closeModal = function () {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
-};
+// const closeModal = function () {
+//   modal.classList.add("hidden");
+//   overlay.classList.add("hidden");
+// };
 
-for (let i = 0; i < btnsOpenModal.length; i++) {
-  btnsOpenModal[i].addEventListener("click", openModal);
-}
-btnCloseModal.addEventListener("click", closeModal);
+// for (let i = 0; i < btnsOpenModal.length; i++) {
+//   btnsOpenModal[i].addEventListener("click", openModal);
+// }
+// btnCloseModal.addEventListener("click", closeModal);
 
-overlay.addEventListener("click", closeModal);
+// overlay.addEventListener("click", closeModal);
 
-document.addEventListener("keydown", function (e) {
-  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
-    closeModal();
-  }
-  // console.log('Escape key was pressed');
-});
+// document.addEventListener("keydown", function (e) {
+//   if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+//     closeModal();
+//   }
+//   // console.log('Escape key was pressed');
+// });
